f5cf2e89009066a685cd62e921bb5525c0678a9b
Sarah Rogers <s.rogers0715@yahoo.com>
2021-03-09 21:00:01 +0000

    Automatically backed up by Learn

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats (name, age, breed) VALUES ("LIL' BUB", 5, "AMERICAN SHORTHAIR");
+
+INSERT INTO cats (name, age, breed) VALUES ("HANNAH", 1, "TABBY");
--- weather/lib/weather.rb
+++
@@ -1,7 +0,0 @@
-require_relative "../lib/weather/version"
-require_relative "../lib/weather/cli"
-
-module Weather
-  class Error < StandardError; end
-  # Your code goes here...
-end
